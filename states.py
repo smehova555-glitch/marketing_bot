09aed9564ecb881b2b31369dc4a8d1ef7e438119
macbook <macbook@MacBook-Air-macbook-2.local>
2026-02-19 21:22:15 +0300

    update states logic

--- states.py
+++ states.py
@@ -2,23 +2,17 @@ from aiogram.fsm.state import StatesGroup, State
 
 
 class Diagnostic(StatesGroup):
-
-    # выбор формата
     format = State()
 
-    # базовая диагностика
+    # short
+    short_role = State()
+    short_strategy = State()
+
+    # full
     role = State()
-    turnover = State()
     strategy = State()
-    channel = State()
+    source = State()
     stability = State()
     geo = State()
-    budget = State()
 
-    # ветвление
-    branch_start = State()
-    branch_growth = State()
-    branch_scale = State()
-
-    # финал
     finished = State()
