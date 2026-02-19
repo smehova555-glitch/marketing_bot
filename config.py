7b5d97f07b78a2c9454bbc0f1e67c66261d82b18
macbook <macbook@MacBook-Air-macbook-2.local>
2026-02-19 20:09:25 +0300

    fix MANAGER_ID import

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-AGENCY_USERNAME = os.getenv("AGENCY_USERNAME", "your_username")
+AGENCY_USERNAME = os.getenv("AGENCY_USERNAME")
+
+MANAGER_ID = os.getenv("MANAGER_ID")
+
+if MANAGER_ID is not None:
+    MANAGER_ID = int(MANAGER_ID)
