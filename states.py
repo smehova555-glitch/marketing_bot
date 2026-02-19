f181fc8b87c1c76eb41416c140bbec60e9b4c1ce
macbook <macbook@MacBook-Air-macbook-2.local>
2026-02-19 20:20:17 +0300

    clean two level architecture

--- states.py
+++ states.py
@@ -2,9 +2,17 @@ from aiogram.fsm.state import StatesGroup, State
 
 
 class Diagnostic(StatesGroup):
+
+    # выбор формата
+    format = State()
+
+    # короткая
+    short_role = State()
+    short_strategy = State()
+
+    # полная
     role = State()
     strategy = State()
     source = State()
     stability = State()
     geo = State()
-    content = State()   # ← ВОТ ЭТО ДОБАВЬ
