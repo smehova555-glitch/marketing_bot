002c2c4eaead76a70567632f1a3167dbcdaf5c91
macbook <macbook@MacBook-Air-macbook-2.local>
2026-02-20 11:06:29 +0300

    fix states structure

--- states.py
+++ states.py
@@ -2,11 +2,18 @@ from aiogram.fsm.state import StatesGroup, State
 
 
 class Diagnostic(StatesGroup):
+
+    # выбор формата
     format = State()
 
+    # короткая версия
+    short_role = State()
+    short_strategy = State()
+
+    # полная версия
     role = State()
     strategy = State()
     source = State()
     stability = State()
-    analytics = State()
+    geo = State()
     budget = State()
